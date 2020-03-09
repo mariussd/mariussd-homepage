a780f2072fe9e4a15eb8e90750e14faf83644aee
mariussd <mariussd@v1041-wn-rt-d-152-51.campus-dynamic.uwaterloo.ca>
2020-03-09 15:36:57 -0400

    Adds some trial-and-error tutorial code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import Vue from "vue";
+import routes from "./routes";
 
-var app = new Vue({
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+let app = new Vue({
   el: "#app",
   data: {
-    message: "Hello Vue!"
+    message: "Hello Vue!",
+    bolle: "Satan",
+    time: "You loaded the page on " + new Date().toLocaleString(),
+    day: "It is " + days[new Date().getDay()] + " my dudes"
+  }
+});
+
+let app5 = new Vue({
+  el: "#app-5",
+  data: {
+    visible: true,
+    message: "Rånk",
+    counter: 0
+  },
+  methods: {
+    toggleMessage: function() {
+      this.visible = !this.visible;
+    },
+    incrementCounter: function() {
+      this.counter = this.counter + 1;
+    },
+    decrementCounter: function() {
+      this.counter = this.counter - 1;
+    }
   }
 });
