2683a87e51c7168a2e9b7e8323d9faa65f9bf6b2
mariussd <marius.s.dreyer@gmail.com>
2021-01-03 15:17:49 +0100

    hehe

--- src/index.js
+++ src/index.js
@@ -8,35 +8,60 @@ const days = [
   "Wednesday",
   "Thursday",
   "Friday",
-  "Saturday"
+  "Saturday",
 ];
+
+let data = {
+  time: "You loaded the page on " + new Date().toLocaleString(),
+  day: "It is " + days[new Date().getDay()] + " my dudes",
+};
 
 let app = new Vue({
   el: "#app",
-  data: {
-    message: "Hello Vue!",
-    bolle: "Satan",
-    time: "You loaded the page on " + new Date().toLocaleString(),
-    day: "It is " + days[new Date().getDay()] + " my dudes"
-  }
+  data: data,
 });
 
 let app5 = new Vue({
   el: "#app-5",
   data: {
     visible: true,
+    imageVisible: false,
     message: "Rånk",
-    counter: 0
+    counter: 0,
   },
   methods: {
-    toggleMessage: function() {
+    toggleMessage: function () {
       this.visible = !this.visible;
     },
-    incrementCounter: function() {
+    toggleImage: function () {
+      this.imageVisible = !this.imageVisible;
+    },
+    incrementCounter: function () {
       this.counter = this.counter + 1;
     },
-    decrementCounter: function() {
+    decrementCounter: function () {
       this.counter = this.counter - 1;
-    }
-  }
+    },
+  },
 });
+
+let watchExampleVM = new Vue({
+  el: "#watch-example",
+  data: {
+    math: "",
+  },
+  computed: {
+    answer: function () {
+      return isNaN(this.math) || this.math === ""
+        ? "Only numbers please"
+        : Math.pow(parseInt(this.math), 2);
+    },
+  },
+});
+
+let tmp = result;
+let fefefffffef = result[1];
+
+console.log("fheufuwdj", users);
+console.log("Moss er best", tmp);
+console.log("pølse i vaffel", JSON.parse(fefefffffef));
